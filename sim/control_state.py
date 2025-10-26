7bf1ad405ac917c52625c00412b147349c365319
SaturnX77 <saturnvdt@gmail.com>
2025-10-25 19:34:18 -0700

    removing sim from hand_gesture branch

--- sim/control_state.py
+++
@@ -1,9 +0,0 @@
-from enums import BotControl, CameraControl
-
-class ControlState:
-    def __init__(self):
-        self.fb_control = BotControl.NEUTRAL
-        self.rl_control = BotControl.NEUTRAL
-        self.cam_control = CameraControl.NONE
-
-state = ControlState()
