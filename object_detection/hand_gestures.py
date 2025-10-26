24a4307ea4ed23ce22ee740c932c140fc812b032
SaturnX77 <saturnvdt@gmail.com>
2025-10-25 19:21:56 -0700

    hand gestures trained model attempt 1

--- object_detection/hand_gestures.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-
-model = YOLO("yolo11n-pose.pt")
-
-results = model.train(data="hand-keypoints.yaml", epochs=100, imgsz=640, device = "mps")
---
+++ object_detection/hand_gestures_train.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+def main():
+    model = YOLO("yolo11n-pose.pt")
+
+    results = model.train(
+        data="hand-keypoints.yaml",
+        epochs=100,
+        imgsz=640,
+        device="0",
+        batch=48,
+        workers=8,  # i7-13700K can handle 12 threads fine
+    )
+
+
+if __name__ == "__main__":
+    main()
+
