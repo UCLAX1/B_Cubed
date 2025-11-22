2cdd04af6d18e472c5b3b1fe7044c3e7d327ebc8
Admin <Admin@THINKPADT14>
2025-11-22 13:36:52 -0800

    small addition to robot.py

--- main/robot.py
+++ main/robot.py
@@ -6,13 +6,11 @@ import time
 
 # https://docs.wpilib.org/en/stable/docs/zero-to-robot/step-4/creating-test-drivetrain-program-cpp-java-python.html
 # https://docs.wpilib.org/en/stable/docs/software/python/subcommands/deploy.html
+# https://robotpy.readthedocs.io/projects/rev/en/stable/rev.html
 
-
-motor = rev.CANSparkMax(1, rev.CANSparkMax.MotorType.kBrushless)
+motor = rev.SparkMax(1, rev.SparkMax.MotorType.kBrushless)
 motor.set(0.5)
 
 time.sleep(2)
 
-
-exit(0)
-
+motor.set(0.0)
