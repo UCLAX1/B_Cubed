98c1408cf5ee29ef778748defd750dcd6fd9623c
SaturnX77 <saturnvdt@gmail.com>
2026-01-31 14:38:16 -0800

    created requirements file + edited launch script to open all processes at once

--- ros2_ws/launch.sh
+++ ros2_ws/launch.sh
@@ -1,5 +1,19 @@
 colcon build --cmake-args=-DCMAKE_BUILD_TYPE=Release --parallel-workers "$(nproc)"
 source install/setup.bash
+
+
+gnome-terminal --title="hand tracking" -- bash -lc "
+  source '/home/jetson-nano-x1/Documents/B_Cubed/ros2_ws/install/local_setup.bash'
+  ros2 run person_tracking hands
+  exec bash
+"
+
+gnome-terminal --title="person tracking" -- bash -lc "
+  source '/home/jetson-nano-x1/Documents/B_Cubed/ros2_ws/install/local_setup.bash'
+  ros2 run person_tracking person
+  exec bash
+"
+
 ros2 launch zed_wrapper zed_camera.launch.py camera_model:=zedm
-#ros2 run person_tracking person
+
 
