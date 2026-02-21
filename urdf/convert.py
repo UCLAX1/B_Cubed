67ed2dc972418ad9cfdddee0bb960f076dcc9b29
YifanFang1 <logan.yifan.fang@gmail.com>
2026-02-21 15:35:14 -0800

    removed extraneous files

--- urdf/convert.py
+++
@@ -1,7 +0,0 @@
-import mujoco
-
-# Load URDF file (make sure path is correct and meshes are reachable)
-model = mujoco.MjModel.from_xml_path("D:/Ballu_export/bb8_car/urdf/bb8_car.urdf")
-
-# Save converted MJCF
-mujoco.mj_saveLastXML("D:/Ballu_export/bb8_car/urdf/bb8_car_converted.xml", model)
