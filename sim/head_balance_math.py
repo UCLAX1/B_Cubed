3798e52ebc02eb7a21f72cea09da13532d1b5640
Terrence Han <147001387+ThisisTerrencesAc@users.noreply.github.com>
2026-05-09 17:20:33 -0700

    New script
    A script that is new

--- sim/head_balance_math.py
+++ sim/head_balance_math.py
@@ -4,7 +4,7 @@ import numpy as np
 def find_motor_angles(pitch, roll, desired_angle):
     pitch = pitch * np.pi/180
     roll = roll * np.pi/180
-    desired_angle = desired_angle * np.pi/180;
+    desired_angle = desired_angle * np.pi/180
     Lazy_Susan = 0
     Arm = 0
     
@@ -12,22 +12,17 @@ def find_motor_angles(pitch, roll, desired_angle):
     n2 = -math.sin(roll)
     n3 = math.cos(pitch) * math.cos(roll)
     
-    Arm = math.acos(n3) * 180/np.pi
-    # Determine sign of Arm based on n1 (forward/backward direction)
-    if n1 > 0:
+    Arm = -math.acos(n3) * 180/np.pi
+    
+    if pitch < 0:
         Arm = -Arm
     
     if roll != 0:
         Lazy_Susan = math.acos(n1/math.sqrt(n1**2 + n2**2)) * 180/np.pi
     
-    if pitch > np.pi: 
+    if roll < 0:
+        Lazy_Susan = -Lazy_Susan
         Arm = -Arm
-    if roll > np.pi: 
-        Lazy_Susan = - Lazy_Susan
-    
-    # Flip directions
-    Arm = -Arm
-    Lazy_Susan = -Lazy_Susan
 
     if (abs(Lazy_Susan) > 90):
         Arm = -Arm
