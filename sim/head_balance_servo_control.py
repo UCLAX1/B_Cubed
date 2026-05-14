a898df865c468070aaa4df65d371da347f56e9a9
Ethan Ton <ethan.m.ton@gmail.com>
2026-05-13 21:18:41 -0700

    Update head_balance_servo_control.py

--- sim/head_balance_servo_control.py
+++ sim/head_balance_servo_control.py
@@ -25,7 +25,7 @@ DESIRED_ANGLE = 0.0
 SETTINGS_FILE = "RTIMULib"
 sys.path.append("/usr/lib/python3/dist-packages")
 import RTIMU
-import ServoEx
+from ServoEx import ServoEx
 
 settings = RTIMU.Settings(SETTINGS_FILE)
 imu = RTIMU.RTIMU(settings)
