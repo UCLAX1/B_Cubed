7c1fc30adefdfaf73a0dc6dc244dba58b17bad54
Yifan Fang <logan.yifan.fang@gmail.com>
2026-05-20 19:56:46 -0700

    mosfet OFF

---
+++ sim/mosfetOFF.py
@@ -0,0 +1,8 @@
+import time
+from gpiozero import DigitalOutputDevice, Servo
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+MOSFET_PIN = 16
+
+mosfet = DigitalOutputDevice(MOSFET_PIN)
+mosfet.off()
