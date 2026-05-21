ea369570caf8458fc5176bb612f3773625a02b51
Yifan Fang <logan.yifan.fang@gmail.com>
2026-05-20 21:21:43 -0700

    12

--- sim/test_head_servo_pin13.py
+++ sim/test_head_servo_pin13.py
@@ -7,7 +7,7 @@ import time
 from gpiozero import DigitalOutputDevice, Servo
 from gpiozero.pins.pigpio import PiGPIOFactory
 
-SERVO_PIN = 27  # Physical pin 32 - previously working
+SERVO_PIN = 12  # Physical pin 32 - previously working
 MOSFET_PIN = 16
 SPIN_SPEED = 0.3
 DURATION = 60.0
