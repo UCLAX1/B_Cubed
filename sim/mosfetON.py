4fd4c511951b6ac9871cde670bc103945b1a05a1
Yifan Fang <logan.yifan.fang@gmail.com>
2026-05-20 20:04:41 -0700

    () added

--- sim/mosfetON.py
+++ sim/mosfetON.py
@@ -4,7 +4,7 @@ from gpiozero.pins.pigpio import PiGPIOFactory
 
 MOSFET_PIN = 16
 
-mosfet = DigitalOutputDevice(MOSFET_PIN, pin_factory = PiGPIOFactory)
+mosfet = DigitalOutputDevice(MOSFET_PIN, pin_factory = PiGPIOFactory())
 
 try:
     mosfet.on()
