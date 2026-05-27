66155bb604036ec798851ba5f9c5f6c60579019a
Yifan Fang <logan.yifan.fang@gmail.com>
2026-05-27 13:49:59 -0700

    debug servos

--- sim/servo_sample.py
+++ sim/servo_sample.py
@@ -4,8 +4,8 @@ from time import sleep
 # Define the servos using their BCM (GPIO) numbers, NOT physical pins!
 # Adjust min_pulse_width and max_pulse_width if your servos don't spin fully
 garosa_5kg = Servo(20, initial_value=None)  # Continuous (Physical Pin 12)
-diymall_70kg = Servo(13, initial_value=None)  # Continuous (Physical Pin 32)
-annimos_150kg = Servo(12, initial_value=None)  # Standard (Physical Pin 33)
+diymall_70kg = Servo(12, initial_value=None)  # Continuous (Physical Pin 32)
+annimos_150kg = Servo(13, initial_value=None)  # Standard (Physical Pin 33)
 MOSFET = DigitalOutputDevice(16)  # MOSFET control (Physical Pin 36)
 
 try:
@@ -15,7 +15,6 @@ try:
     print("Testing Standard Servo (150KG)")
     annimos_150kg.value = 0.1 # Move to center position
     sleep(2)
-    annimos_150kg.value = 0
     
     print("Testing Continuous Servo (70KG)")
     diymall_70kg.value = 0.1 # Spin forward at 50% speed
